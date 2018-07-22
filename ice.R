4a1ecfcd78fda44075cdea53d76caba1c06213fd
benmatheson <mathesonben@gmail.com>
2018-07-22 10:45:11 -0800

    initial

---
+++ ice.R
@@ -0,0 +1,16 @@
+library(tidyverse, jsonlite)
+library(jsonlite)
+setwd("projects/ice)
+getwd();
+iceRaw <- read_csv("data/b285fe9a-2c1d-420c-814e-aea420eac979_GovernmentSpendingContracts-2017")
+
+
+
+
+
+
+
+
+
+
+write_json(weedRaw, "weed.json")
